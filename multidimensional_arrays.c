d3ee23feb88bf2a6be04acc3eba6a3e7489f7bb4
salamanderctesiphon <swbrookshire@gmail.com>
2024-01-01 15:56:26 -0600

    created a file for working with multidimensional arrays

---
+++ multidimensional_arrays.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/* examples of multidimensional arrays
+  type name[size1][size2]...[sizeN];
+  int foo[1][2][3];
+  char vowels[1][5] = {
+    {'a', 'e', 'i', 'o', 'u'}
+  };
+*/
+
+int main()
+{
+
+  return 0;
+}
